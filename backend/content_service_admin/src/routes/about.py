f718c6d275dbb1ceac18c2c0a4047ab59749ca01
IvanZaycev0717 <ivzaycev0717@yandex.ru>
2025-09-03 16:36:02 +0300

    feat(content_service_admin): add form for create about

--- backend/content_service_admin/src/routes/about.py
+++ backend/content_service_admin/src/routes/about.py
@@ -1,7 +1,7 @@
 import logging
-from typing import List, Optional
+from typing import Annotated, Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Query, Request, status
+from fastapi import APIRouter, Depends, HTTPException, Query, Request, status, Form
 
 
 from services.crud.about import AboutCRUD
@@ -37,7 +37,33 @@ async def get_about_content(
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_about_content(
-    request: CreateAboutRequest, about_crud: AboutCRUD = Depends(get_about_crud)
+    image: str = Form(
+        description="URL изображения", json_schema_extra={"example": "image_N.jpg"}
+    ),
+    title_en: str = Form(
+        description="Заголовок на английском",
+        json_schema_extra={"example": "Some title 1 EN"},
+    ),
+    description_en: str = Form(
+        description="Описание на английском",
+        json_schema_extra={"example": "Some description 1 EN"},
+    ),
+    title_ru: str = Form(
+        description="Заголовок на русском",
+        json_schema_extra={"example": "Название 1 RU"},
+    ),
+    description_ru: str = Form(
+        description="Описание на русском",
+        json_schema_extra={"example": "Описание 1 RU"},
+    ),
+    about_crud: AboutCRUD = Depends(get_about_crud),
 ):
-    result = await about_crud.create(request.model_dump(exclude_none=True))
-    return f"A new document with _id={result} was created"
+    data = CreateAboutRequest(
+        image=image,
+        translations={
+            "en": {"title": title_en, "description": description_en},
+            "ru": {"title": title_ru, "description": description_ru},
+        },
+    )
+    result = await about_crud.create(data.model_dump(exclude_none=True))
+    return f"Document created with _id={result}"
