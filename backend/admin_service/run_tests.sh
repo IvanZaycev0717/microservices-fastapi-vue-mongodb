4c857bf775a80b4a73ab5d2b35bb2223c2ca9ca3
IvanZaycev0717 <ivzaycev0717@yandex.ru>
2025-10-03 11:31:40 +0300

    test(admin_service): add all the tests Postman & pytest script

---
+++ backend/admin_service/run_tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Run Pytest"
+python -m pytest -v
+
+echo "Run FastAPI Backend"
+cd src
+python main.py &
+BACKEND_PID=$!
+cd ..
+sleep 5
+
+echo "Running Postman Tests"
+npx newman run "Content_Admin_API.postman_collection.json" \
+  -e "Content_Admin_Devs.postman_environment.json" \
+  --global-var "test_files=$PWD/postman-files" \
+  --reporters="cli,htmlextra" \
+  --reporter-htmlextra-export="newman_report.html" \
+  --reporter-cli-no-success-assertions \
+  --reporter-cli-no-console
+NEWMAN_EXIT=$?
+
+
+kill $BACKEND_PID
+
+
+if [ $NEWMAN_EXIT -eq 0 ]; then
+    echo "✅ All tests passed!"
+else
+    echo "❌ Some tests failed - see newman_report.html for details"
+fi
+
+exit $NEWMAN_EXIT
